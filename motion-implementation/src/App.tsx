0c36c661be404e039d7b4a69bb867f4ce7ba09a3
Ciaran <cslemon96@gmail.com>
2022-07-24 16:29:30 +0200

    Added motion implementation

--- motion-implementation/src/App.tsx
+++ motion-implementation/src/App.tsx
@@ -1,34 +1,110 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {useState} from 'react';
+import {motion} from 'framer-motion';
+
+const randN = () => {
+  return Math.floor(Math.random() * 10);
+};
+
+const randF = (min, max) => {
+  return Math.random() * (max - min + 1) + min;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [board, setBoard] = useState([]);
+  const size = 64;
+  const cols = size / 8;
+
+  for (let i = 0; i < size; i++) {
+    let n = randN().toString();
+    board.length < size && setBoard(b => [...b, n]);
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Terminal>
+      <NumberGrid {...{cols}}>
+        {board.map(n => (
+          <Sprite {...{n}} />
+        ))}
+      </NumberGrid>
+    </Terminal>
+  );
 }
 
-export default App
+const Sprite = ({n}: {n: string}) => {
+  const scales = [];
+  const rots = [];
+  const ops = [];
+
+  for (let i = 0; i < 24; i++) {
+    scales.push(randF(0.8, 1));
+    rots.push(randF(-20, 20));
+    ops.push(randF(0.8, 1));
+  }
+
+  const trs = {
+    ease: 'easeInOut',
+    repeat: Infinity,
+  };
+
+  return (
+    <div>
+      <motion.div
+        className="h-12 w-12 flex justify-center items-center"
+        initial={{
+          scale: 1,
+          rotate: 0,
+        }}
+        animate={{
+          scale: scales,
+          rotate: rots,
+          opacity: ops,
+          transition: {
+            duration: 60,
+            ...trs,
+          },
+        }}
+        whileHover={{
+          // Scale on Hover
+          scale: randF(1.3, 1.8),
+          transition: {
+            duration: randF(0.2, 0.4),
+            ...trs,
+          },
+        }}
+      >
+        <p className="text-md select-none inline-block">{n}</p>
+      </motion.div>
+    </div>
+  );
+};
+
+const NumberGrid = ({cols, ...props}: {cols: number}) => {
+  const colMap = {
+    4: 'grid-cols-4',
+    8: 'grid-cols-8',
+    16: 'grid-cols-16',
+    32: 'grid-cols-32',
+  };
+
+  return (
+    <div className={`grid ${colMap[cols]}`} id="grid">
+      {props.children}
+    </div>
+  );
+};
+
+const Terminal = ({...props}) => {
+  return (
+    <>
+      <div className="screen absolute mix-blend-overlay w-screen h-screen" />
+      <div
+        id="terminal"
+        className="text-center bg-gradient-to-b from-zinc-900/90 to-zinc-900 text-cyan-100 w-screen h-screen flex items-center justify-center"
+      >
+        {props.children}
+      </div>
+    </>
+  );
+};
+
+export default App;
